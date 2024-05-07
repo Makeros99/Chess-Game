844fba056dad538221aadb1ccb2d025dc84bd014
Cüneyt <120556564+Makeros99@users.noreply.github.com>
2024-05-07 03:03:15 +0300

    Add files via upload

---
+++ src/Game.js
@@ -0,0 +1,45 @@
+import  {Chess} from 'chess.js'
+import {BehaviorSubject} from 'rxjs'
+
+const chess = new Chess();
+
+const subjectGame = new BehaviorSubject()
+
+export default subjectGame
+
+export const initGame = () => {
+    updateGame();
+}
+
+export const move = (from , to) => {
+    console.log(from,to);
+    const moveOperation = chess.move({from , to})
+    if(moveOperation){
+        updateGame();
+    }
+
+}
+
+const updateGame =() => {
+    const isGameOver = chess.isGameOver();
+    subjectGame.next({chess: chess.board() , isGameOver, result: isGameOver ? getGameResult() : null})
+
+}
+const getGameResult = () => {
+    if(chess.isCheckmate()){
+    const winner = chess.turn() === 'w' ? 'Black' : 'White';
+    return `Şah Mat - Kazanan ${winner}`
+    }else if(chess.isDraw()){
+        let reason = "50 Hamle Kuralı";
+        if(chess.isStalemate()){
+            reason = 'Çıkmaz Döngü'
+        }else if(chess.isThreefoldRepetition()){
+            reason = "Tekrarlayan Hamleler"
+        }else if(chess.isInsufficientMaterial()){
+            reason = "Yetersiz Taş Sayısı"
+        }
+        return reason
+    }else{
+        return 'Bilinmeyen Durum'
+    }
+}
---
+++ src/SquareBoard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { DragPreviewImage, useDrag } from 'react-dnd'
+
+const SquareBoard = ({brd , positionCntrl}) => {
+
+    const [{isDragging}, drag, preview] = useDrag({
+      type : 'chess',
+      item: { id: `${positionCntrl}_${brd.type}_${brd.color}` }, 
+      collect: (monitor) => {
+        return {isDragging : !!monitor.isDragging}
+      }
+    })
+
+    const pieceImage = require(`../public/assets/images/${brd.type}_${brd.color}.png`) 
+    console.log( brd , "brd")
+  return (
+    <div ref={drag}>
+      <DragPreviewImage src={pieceImage} connect={preview} />
+      <img src={pieceImage} alt="" />
+    </div>
+  )
+}
+
+export default SquareBoard
